a9d4d941474957e29addae8987c2314673ed1c66
Luke Chadwick <luke@vertis.io>
2024-01-06 12:15:03 +0200

    chore: make it export everything by default

--- firestore/firestore2json.ts
+++ firestore/firestore2json.ts
@@ -1,42 +1,50 @@
-import { getFirestoreInstance, cleanUp, writeRecord } from "./utils";
+import {
+  getFirestoreInstance,
+  cleanUp,
+  writeRecord,
+  listCollections,
+} from "./utils";
 import * as fs from "fs";
 const args = process.argv.slice(2);
 
-let processDocument;
-if (fs.existsSync(`./firestore/${args[0]}.ts`)) {
-  // read file to string
-  processDocument = require(`./${args[0]}.ts`).default;
-  console.log(processDocument);
-} else {
-  console.log("No processDocument file found for collection: " + args[0]);
-  //   process.exit(1);
-}
+// let processDocument;
+// if (fs.existsSync(`./firestore/${args[0]}.ts`)) {
+//   // read file to string
+//   processDocument = require(`./${args[0]}.ts`).default;
+//   console.log(processDocument);
+// } else {
+//   console.log("No processDocument file found for collection: " + args[0]);
+//   //   process.exit(1);
+// }
 
 let db;
 
 const recordCounters = {};
 let limit = 0;
 
-if (args.length < 1) {
-  console.log(
-    "Usage: firestore2json.ts <collectionName> [<batchSize>] [<limit>]"
-  );
-  process.exit(1);
-} else {
+async function main() {
   db = getFirestoreInstance();
-  main(args[0], args[1] || "1000", args[2] || "0");
+  console.log(listCollections);
+  const collections = args[0] ? args[0].split(",") : await listCollections();
+  processExport({ collections, batchSize: 1000, limit: limit });
+}
+main();
+
+async function processExport({
+  collections,
+  batchSize,
+  limit,
+}: {
+  collections: string[];
+  batchSize: number;
+  limit: number;
+}) {
+  for (const collectionName of collections) {
+    await getCollection(collectionName, 0, batchSize, limit);
+  }
 }
 
-async function main(collectionName: string, batchSize: string, limit: string) {
-  // if (fs.existsSync(`./${collectionName}.json`)) {
-  //     console.log(`${collectionName}.json already exists, aborting...`);
-  //     process.exit(1);
-  // } else {
-  await getAll(collectionName, 0, parseInt(batchSize), parseInt(limit));
-  // }
-}
-
-async function getAll(
+async function getCollection(
   collectionName: string,
   offset: number,
   batchSize: number,
@@ -57,7 +65,7 @@ async function getAll(
     return;
   }
   if (data.length > 0) {
-    await getAll(collectionName, offset + data.length, batchSize, limit);
+    await getCollection(collectionName, offset + data.length, batchSize, limit);
   } else {
     cleanUp(recordCounters);
     for (let key in recordCounters) {
@@ -106,10 +114,10 @@ async function getBatch(
       else if (!doc.firestoreid) doc.firestoreid = fsdoc.id;
       else if (!doc.original_id) doc.original_id = fsdoc.id;
       else if (!doc.originalid) doc.originalid = fsdoc.id;
-      console.log("processDocument", typeof processDocument);
-      if (processDocument) {
-        doc = processDocument(collectionName, doc, recordCounters, writeRecord);
-      }
+      // console.log("processDocument", typeof processDocument);
+      // if (processDocument) {
+      //   doc = processDocument(collectionName, doc, recordCounters, writeRecord);
+      // }
       console.log(doc);
       writeRecord(collectionName, doc, recordCounters);
       data.push(doc);
